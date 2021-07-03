f6d3f6b3e658c7f1ca37726c4f29809e570af599
dev2820 <second28200@gmail.com>
2021-07-03 14:40:28 +0900

    react router 분리중 (FindPass,Home,Login,NotFound,Signup,HelloWorld 생성)

---
+++ frontend/src/components/pages/FindPass/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class FindPass extends React.Component {
+    render() {
+        return (
+            <div>
+                findpass
+            </div>
+        )
+    }
+}
+
+export default FindPass;
---
+++ frontend/src/components/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                Login
+            </div>
+        )
+    }
+}
+
+
+export default Login;
