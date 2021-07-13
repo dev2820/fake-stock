fbf300f955813bed1380d3a7b7c58ccaaa94c409
Tuna-is-DW <ydw5210@gmail.com>
2021-07-14 01:43:20 +0900

    login폼에서 isLogiined state로 로그인 여부 확인하도록 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@ import "./App.css";
 import routes from "./routes.js";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducers from "./modules";
 function App() {
-  return <Provider store={createStore(reducers)}>{routes}</Provider>;
+  return (
+    <Provider store={createStore(reducers, composeWithDevTools())}>
+      {routes}
+    </Provider>
+  );
 }
 
 export default App;
