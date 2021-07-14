58280e3f471251032f830dba45893a10205053a8
spaff13 <spaff316@gmail.com>
2021-07-14 19:49:37 +0900

    updateMiddleWare 수정

--- backend/my_modules/jwt.js
+++ backend/my_modules/jwt.js
@@ -59,7 +59,7 @@ module.exports.jwtCheckMiddleWare = (req, res, next)=>{
 module.exports.updatePwMiddleWare = (req, res, next)=>{
 	if(!req.signedCookies.findpass)
 		next();
-	jwt.verify(req.signedCookies.refresh, suuuuperSecret, (err, decoded)=>{
+	jwt.verify(req.signedCookies.findpass, suuuuperSecret, (err, decoded)=>{
 		req.signedCookies.refresh = this.createRefreshJwt(decoded.id);
 		next();
 	})
