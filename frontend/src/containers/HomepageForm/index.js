ec68abb2a5309010f5545cf809b083d0133d98a0
Tuna-is-DW <ydw5210@gmail.com>
2021-07-14 16:30:54 +0900

    redirect containers에서 pages로 변경 및 isLogined->accessToken으로 변경

--- frontend/src/containers/HomepageForm/index.js
+++ frontend/src/containers/HomepageForm/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
+// import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 // import userReducer from "../../modules/userReducer";
 
 const HomePage = () => {
-  const { isLogin } = useSelector(({ userReducer }) => ({
-    isLogin: userReducer.isLogined,
-  }));
+  // const { isAccessToken } = useSelector(({ userReducer }) => ({
+  //   isAccessToken: userReducer.accessToken,
+  // }));
   return (
     <div>
-      {isLogin || <Redirect to="/login" />}
+      {/* {isAccessToken || <Redirect to="/login" />} */}
       <div>
         <h1>Welcome to Fake-Stock!</h1>
       </div>
--- frontend/src/pages/SignupPage/index.js
+++ frontend/src/pages/SignupPage/index.js
@@ -1,28 +1,29 @@
-import './signupPage.css'
-import React from 'react';
-import SignupForm from '../../containers/SignupForm';
-import { Redirect } from 'react-router-dom'
+import "./signupPage.css";
+import React from "react";
+import SignupForm from "../../containers/SignupForm";
+import { Redirect } from "react-router";
+import { connect } from "react-redux";
 import { fetchAccessTokenActionCreator } from "../../modules/userReducer";
-import Wave from '../../components/Wave';
+import Wave from "../../components/Wave";
 class SignupPage extends React.Component {
-    render() {
-        return (
-            <div>
-                <Wave></Wave>
-                <div className="center">
-                    <SignupForm/>
-                </div>
-                {!this.props.accessToken && <Redirect to="/" />}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Wave />
+        <div className="center">
+          <SignupForm />
+        </div>
+        {!this.props.accessToken === null && <Redirect to="/" />}
+      </div>
+    );
+  }
 }
 /*redux 연결 */
 export default connect(
-    ({ userReducer }) => ({
-        accessToken: userReducer.accessToken,
-    }),
-    {
-        fetchAccessTokenActionCreator,
-    }
+  ({ userReducer }) => ({
+    accessToken: userReducer.accessToken,
+  }),
+  {
+    fetchAccessTokenActionCreator,
+  }
 )(SignupPage);
