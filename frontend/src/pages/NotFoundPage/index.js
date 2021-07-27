0c92ef995f3106c5f2c13efef5c314e4cf3c81bb
dev2820 <second28200@gmail.com>
2021-07-27 14:48:49 +0900

    404 accessToken 없으면 login으로 이동하도록 수정 && React,Fragment 적용

--- frontend/src/pages/NotFoundPage/index.js
+++ frontend/src/pages/NotFoundPage/index.js
@@ -1,11 +1,17 @@
+import React from 'react'
 import { useLocation,Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Redirect } from "react-router";
 const NotFoundPage = (props) => {
     const goBack = () => {
         props.history.goBack();
     }
     const location = useLocation();
+    const { isAccessToken } = useSelector(({ userReducer }) => ({
+        isAccessToken: !!userReducer.accessToken,
+    }));
     return (
-        <div>
+        <React.Fragment>
             <h2>404</h2>
             <h3>Oops... page not found</h3> 
             <p>we can't find page "{location.pathname}". please check your url</p>
@@ -13,8 +19,9 @@ const NotFoundPage = (props) => {
                 <Link to='/'>Go Home</Link>
             </button>
             <button onClick={goBack}>Go Back</button>
-        </div>
+            {isAccessToken && <Redirect to="/" />}
+        </React.Fragment>
     )
-}
+}  
 
 export default NotFoundPage;
